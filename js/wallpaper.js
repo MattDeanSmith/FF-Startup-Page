6aa24c34886949699f71c4a7f3ac83367d9c9f0b
Matthew Smith <mattdeansmith@hotmail.co.uk>
2016-10-31 14:01:57 +0000

    Change wallpaper per day.

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -9,36 +9,36 @@ function init() {
   // Monday
   if(day == 'Monday') {
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(images/wallpapers/monday.jpg)';;
+    body.style.backgroundImage = 'url(images/wallpapers/monday.jpg)';
   };
 
   // Tuesday
   if(day == 'Tuesday') {
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(images/wallpapers/tuesday.jpg)';;
+    body.style.backgroundImage = 'url(images/wallpapers/tuesday.jpg)';
   };
 
   // Wednesday
   if(day == 'Wednesday') {
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(images/wallpapers/wednesday.jpg)';;
+    body.style.backgroundImage = 'url(images/wallpapers/wednesday.jpg)';
   }
 
   // Thursday
   if(day == 'Thursday') {
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(images/wallpapers/thursday.jpg)';;
+    body.style.backgroundImage = 'url(images/wallpapers/thursday.jpg)';
   };
 
   // Friday
   if(day == 'Friday') {
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(images/wallpapers/friday.jpg)';;
+    body.style.backgroundImage = 'url(images/wallpapers/friday.jpg)';
   };
 
   // Saturday
   if(day == 'Saturday') {
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(images/wallpapers/saturday.jpg)';;
+    body.style.backgroundImage = 'url(images/wallpapers/saturday.jpg)';
   };
 };
